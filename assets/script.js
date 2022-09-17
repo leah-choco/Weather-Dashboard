a22bd023b8c457e48b4277f5686759689da6c14b
Your Name <you@example.com>
2022-09-16 21:06:09 -0500

    Modifications to JS after tutoring session

--- assets/script.js
+++ assets/script.js
@@ -1,102 +1,68 @@
-
-
 // OpenWeather API(LearningBot Key, mine would not work)
-const apiKey = "fc02d6d30dd32f5567b9706fc1c7ef42";
-    
-
-  
+//const apiKey = "fc02d6d30dd32f5567b9706fc1c7ef42";
+const apiKey = "01799a4d0b2dee08aa5540200bac64d8";
 
 // Store past searched cities
 let pastCities = [];
 
-  
-
-   
-
 // Load events from local storage
 function loadCities() {
-const storedCities = JSON.parse(localStorage.getItem('pastCities'));
-    if (storedCities) {
+  const storedCities = JSON.parse(localStorage.getItem("pastCities"));
+  if (storedCities) {
     pastCities = storedCities;
- }
+  }
 }
 
 // Store searched cities in local storage
 function storeCities() {
-        localStorage.setItem('pastCities', JSON.stringify(pastCities));
+  localStorage.setItem("pastCities", JSON.stringify(pastCities));
 }
 
 // Functions to build the URL for the OpenWeather API call
- 
+
 function buildURLFromInputs(city) {
-    if (city) {
-         return `http://api.openweathermap.org/data/2.5/weather?id=${city}&appid=${apiKey}`;
-    }
+  if (city) {
+    return `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=imperial`;
+  }
 }
 
 function buildURLFromId(id) {
-     return `https://api.openweathermap.org/data/2.5/weather?id=${id}&appid=${apiKey}`;
- 
+  return `https://api.openweathermap.org/data/2.5/weather?id=${id}&appid=${apiKey}`;
 }
 // Function to display the last 5 searched cities
-function displayCities(pastCities) {
-    
-    
+function displayCities(pastCities) {}
+
 // Function to color the UV Index based on EPA color scale
 function setUVIndexColor(uvi) {
-    if (uvi < 3) {
-        return 'green';
-    } else if (uvi >= 3 && uvi < 6) {
-        return 'yellow';
-    } else if (uvi >= 6 && uvi < 8) {
-        return 'orange';
-    } else if (uvi >= 8 && uvi < 11) {
-        return 'red';
-    } 
+  if (uvi < 3) {
+    return "green";
+  } else if (uvi >= 3 && uvi < 6) {
+    return "yellow";
+  } else if (uvi >= 6 && uvi < 8) {
+    return "orange";
+  } else if (uvi >= 8 && uvi < 11) {
+    return "red";
+  }
 }
 
 // Search for weather conditions by calling the OpenWeather API
-function searchWeather(){
+function searchWeather(URL) {
+    fetch(URL).then(function(response){
+        console.log(response)
+        return response.json()
+    }) .then(function(data){
+        console.log(data)
+    })
 }
 
-
 // Call OpenWeather API OneCall with lat and lon to get the UV index and 5 day forecast
-let lat = response.coord.lat;
-let lon = response.coord.lon;
-let latLon = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${apiKey}`;
-          
-
-};
-    
-
+/*let lat = response.coord.lat;
+  let lon = response.coord.lon;
+  let latLon = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${apiKey}`;*/
 // Function to display the last searched city
 function displayLastSearchedCity() {
- 
- 
-    // Click handler for search button
-    $('#search-btn').on('click', function (event) {
-        // Preventing the button from trying to submit the form
-        event.preventDefault();
-
-        // Retrieving and scrubbing the city from the inputs
-       
-
-        // Clear the input fields
-        cityInput.val('');
-
-        // Build the query url with the city and searchWeather
-        if (city) {
-            let queryURL = buildURLFromInputs(city);
-            searchWeather(queryURL);
-        }
-    }); 
-    
-    // Click handler for city buttons to load that city's weather
-    $(document).on("click", "button.city-btn", function (event) {
-       
-    });
-
- 
+  // Click handler for city buttons to load that city's weather
+  $(document).on("click", "button.city-btn", function (event) {});
 }
 // load any cities in local storage into array
 loadCities();
@@ -105,6 +71,24 @@ displayCities(pastCities);
 // Display weather for last searched city
 displayLastSearchedCity();
 
+$("#search-btn").on("click", function (event) {
+  // Preventing the button from trying to submit the form
+  event.preventDefault();
+  console.log("I was clicked")
 
+  // Retrieving and scrubbing the city from the inputs
+  var cityInput = $("#city-input");
+  console.log(cityInput)
+  var city = cityInput.val();
+  console.log(city)
 
+  // Build the query url with the city and searchWeather
+  if (city) {
+    let queryURL = buildURLFromInputs(city);
+    console.log(queryURL);
+    searchWeather(queryURL);
+  }
 
+  // Clear the input fields
+  cityInput.val("");
+});
