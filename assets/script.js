69bfa07077fb9a01af0a7d3c94ff78ebd77395c9
Your Name <you@example.com>
2022-09-21 16:24:53 -0500

    fixed search button, writing more JS

--- assets/script.js
+++ assets/script.js
@@ -1,92 +1,109 @@
 //My authoried OpenWeather API key
 var apiKey = "01799a4d0b2dee08aa5540200bac64d8";
-var searchedCity ="";
-var currentCity="";
+//Using "var" to create global variables
+var currentCity ="";
+var lastCity="";
 
 //This function gets the conditons from the API and displays it
-var fetchCurrentWeather= (event) => {
-  let city = $("#search-city").val();
-  currentCity = $('#search-city').val();
-  var mainURL = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=imperial`;
+var getCurrentConditions = (event) => {
+  let city = $("#city-search").val();
+  currentCity = $('#city-search').val();
+  var mainURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=imperial`;
   fetch(mainURL)
   .then((response) => {
-    console.log(response)
     return response.json()
-
   })
   .then((response) => {
     //localStorage
     saveCity(city);
     $("#search-error").text("");
-  })
-}
 
-//URL for icons for the current weather displayed
-var iconsCurrentWeather=//"https://openweathermap.org/img/w/" + weather[0].icon + ".png";
-console.log(iconsCurrentWeather)
+    //URL for icons for the current weather displayed
+    let currentWeatherIcon="https://openweathermap.org/img/w/" + response.weather[0].icon + ".png";
+    console.log(iconsCurrentWeather)
 
 
-//Formatting timezone 
-let currentTime = response.dt;
-let timeZone = response.timezone;
-let currentHours = timeZone /60 /60;
-let currentMoment = moment.unix(currentTime).utc().utcOffset(currentHours);
+    //Formatting timezone 
+    let currentTimeUTC = response.dt;
+    let currentTimeZoneOffset = response.timezone;
+    let currentTimeZoneOffsetHours = currentTimeZoneOffset / 60 / 60;
+    let currentMoment = moment.unix(currentTimeUTC).utc().utcOffset(currentTimeZoneOffsetHours);
 
-displayCities();
+    renderCities();
 
-//"5 day" for the city that was searched
-getForecastFiveDays(event);
+    //"5 day" for the city that was searched
+    getFiveDayForecast(event);
 
-//innerHTML for search results
-let htmlCurrentWeather = `
-  <h3>${response.name} ${currentMoment.format("(MM/DD/YY)")}<img src="${currentWeatherIcon}"></h3>
-  <ul class="list-unstyled">
-    <li>Temperature: ${response.main.temp}&#8457;</li>
-    <li>Humidity: ${response.main.humidity}%</li>
-    <li>Wind Speed: ${response.wind.speed} mph</li>
-    <li id="uvColors"> UV Index:</li>
-  </ul>`;
+    //innerHTML for search results
+    let htmlCurrentWeather = `
+      <h3>${response.name} ${currentMoment.format("(MM/DD/YY)")}<img src="${currentWeatherIcon}"></h3>
+      <ul class="list-unstyled">
+        <li>Temperature: ${response.main.temp}&#8457;</li>
+        <li>Humidity: ${response.main.humidity}%</li>
+        <li>Wind Speed: ${response.wind.speed} mph</li>
+        <li id="uvColors"> UV Index:</li>
+      </ul>`;
+      //Appending the results to the DOM
+    $("#currrent-weather").html(htmlCurrentWeather);
 
-  //Appending the results to the DOM
-  $("#currrent-weather").html(htmlCurrentWeather);
-
-  //Call OpenWeather API OneCall with lat and lon to get the UV index
-  let lat = response.coord.lat;
-  let lon = response.coord.lon;
-  let latLonURL = `https://api.openweathermap.org/data/2.5/uvi?lat=${lat}&lon=${lon}&appid=${apiKey}`
+    //Call OpenWeather API OneCall with lat and lon to get the UV index
+    let latitude = response.coord.lat;
+    let longitude = response.coord.lon;
+    let latLonURL = `https://api.openweathermap.org/data/2.5/uvi?lat=${lat}&lon=${lon}&appid=${apiKey}`;
   
-  fetch(latLonURL)
-  .then((response) => {
+    fetch(latLonURL)
+    .then((response) => {
     return response.json();
   })
   .then((response) => {
-  // Function to color the UV Index based on EPA color scale
-  let setUVIcolor=(uvi) =>{
-  if (uvi < 3) {
-    return "green";
-  } else if (uvi >= 3 && uvi < 6) {
-    return "yellow";
-  } else if (uvi >= 6 && uvi < 8) {
-    return "orange";
-  } else if (uvi >= 8 && uvi < 11) {
-    return "red";
+  let uvIndex= response.value;
+  $("#uvIndex").html(`UV Index: <span id="uvVal"> ${uvIndex}</span>`);
+  if ( uvIndex>=0 && uvIndex<3) {
+      $("#uvVal").attr("class", "uv-favorable");
+  } else if (uvIndex>=3 && uvIndex<8) {
+      $("#uvVal").attr("class", "uv-moderate");
+  } else if (uvIndex>=8) {
+      $("#uvVal").attr("class", "uv-severe");
   }
-}
-  })
+
+});
 
 
 // Search for weather conditions by calling the OpenWeather API
-/*function searchWeather(URL) {
-    fetch(URL).then(function(response){
-        console.log(response)
-        return response.json()
-    }) .then(function(data){
-        console.log(data)
-    })
-}*/
+var getFiveDayForecast = (event) => {
+    let city = $("#city-search").val();
 
 
-// Function to display the last searched city
+    let queryURL = "https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=imperial";
+
+    fetch(queryURL)
+      .then((response)=>{
+        return response.json();
+      })
+      .then((response)=> {
+        //template for HTML
+        let fiveDayForecastHTML = `
+        <h2>5 Day Forecast:</h2>
+        <div class="d-inline-flex flex-wrap" id="fiveDayForecastUl">`;
+
+
+        //Loop through cities and times
+        for (let i=0; i < response.list.length; i++){
+          let dayData = response.list[i];
+          let dayTimeUTC = dayData.dt;
+          let timeZoneOffset = response.city.timezone;
+          let timeZoneOffsetHours = timeZoneOffset / 60/ 60;
+          let thisMoment= moment.unix(dayTimeUTC).utc().utcOffset(timeZOneOffsetHours);
+          let iconURL = "https://openweathermap.org/img/w/" + dayData.weather[0].icon + ".png";
+        }
+      })
+
+      //Format HTML elements
+
+}
+
+
+/*// Function to display the last searched city
 function displayLastSearchedCity() {
   // Click handler for city buttons to load that city's weather
   $(document).on("click", "button.city-btn", function (event) {});
@@ -118,4 +135,4 @@ $("#search-btn").on("click", function (event) {
 
   // Clear the input fields
   cityInput.val("");
-});
+});*/
